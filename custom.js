696f7fde4b4d287fc11c57b1c8d032608ff2c2b1
jahid hassan <jahidhassandiu91@gmail.com>
2019-10-12 01:42:12 +0600

    gomag web templete markub and design complete

---
+++ custom.js
@@ -0,0 +1,14 @@
+$('.toTop').smoothScroll();
+
+$(window).scroll(function () {
+    var top = $(window).scrollTop();
+
+    if (top > 300) {
+        $('.toTop').fadeIn(500);
+    } else {
+        $('.toTop').fadeOut(500);
+    }
+
+});
+
+$(".group1").colorbox({rel:'group1'});
